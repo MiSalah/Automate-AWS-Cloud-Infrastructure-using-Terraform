35d48fcb46618a3ecc850d9f66818de55b1773ca
MiSalah <elbaidoury.salaheddine@gmail.com>
2021-11-13 10:54:48 +0100

    Automating the Creation of  an EC2 Instance using terraform

--- main.tf
+++ main.tf
@@ -2,5 +2,16 @@
 provider "aws" {
     region = "us-east-1"
     access_key = "$access_key"
-    secret_key = "$secrey_key"
+    secret_key = "$secret_key"
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "ubuntu-server" {
+    ami = " ami-083654bd07b5da81d" # The ami id from my aws account
+    instance_type = "t2.micro" 
+
+    tags = {
+      "From" = "Terraform"
+    }
+  
 }
