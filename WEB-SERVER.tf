917261dc7ff4467d80c3b18ba6071b34ae80a375
MiSalah <74468388+MiSalah@users.noreply.github.com>
2021-11-13 18:20:49 +0100

    Subnet & Route table association

--- WEB-SERVER.tf
+++ WEB-SERVER.tf
@@ -22,10 +22,44 @@ resource "aws_internet_gateway" "Internet_Gatewey" {
     }
 }
 
-# 2. Create Internet Gateway
 # 3. Create Custom Route Table
+resource "aws_route_table" "Route_Table" {
+  vpc_id = aws_vpc.Production_VPC.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Internet_Gatewey.id
+  }
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_internet_gateway.Internet_Gatewey.id
+  }
+  tags = {
+    Name = "Production Route table"
+  }
+}
+
 # 4. Create a Subnet
+resource "aws_subnet" "Production_Subnet" {
+    vpc_id = aws_vpc.Production_VPC.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us-east-1a"
+    tags = {
+      Name = "Production Subnet"
+    }
+}
+
 # 5. Associate subnet with Route table
+
+resource "aws_route_table_association" "Subnet_route_table" {
+    subnet_id = aws_subnet.Production_Subnet.id
+    route_table_id = aws_route_table.Route_Table.id
+    tags = {
+        Name = "Subnet & Route table Association"
+    }
+  
+}
+
 # 6. Create Security Group to enable 22, 80, 443 ports (ssh,http,https)
 # 7. Create a network Interface with an ip address in the subnet that was created in step 4
 # 8. Assign an elastic ip address to the network interface created in step 7
