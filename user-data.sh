759a0658152d45591820e823104e3d9a4779cd2e
Salaheddine EL BAIDOURY <74468388+MiSalah@users.noreply.github.com>
2021-11-14 23:21:15 +0100

    bash commands

--- user-data.sh
+++ user-data.sh
@@ -1,1 +1,5 @@
-
+#!/bin/nbash
+sudo apt-get update -y
+sudo apt-get install apache2 -y
+sudo systemctl start apache2 
+sudo bash -c 'echo A Web Server established using Terraform > /var/www/html/index.html'
